176e5fd39e5087f6d9828e959b72d12e9dea2893
Taylus <dragondude@gmail.com>
2017-08-30 19:07:20 -0500

    Initial commit

---
+++ GoogleAssistantWebApi/HelloController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Web.Http;
+
+namespace GoogleAssistantWebApi
+{
+    public class HelloController : ApiController
+    {
+        [HttpGet, HttpPost, Route("")]
+        public object Main([FromBody] dynamic request)
+        {
+            string intent = GetIntentFromRequest(request);
+            switch (intent)
+            {
+                case "WebHookHello": return SayHello();
+                case "WebHookDiagnostics": return SayDiagnostics();
+                default: return SayUnknownIntent();
+            }
+        }
+
+        private string GetIntentFromRequest(dynamic request)
+        {
+            return request?.result?.metadata?.intentName;
+        }
+
+        private object SayHello()
+        {
+            return new { speech = "Royal's web service says hello!" };
+        }
+
+        private object SayDiagnostics()
+        {
+            return new { speech = $"This app is communicating with a web service over HTTPS which is running on a server named {Environment.MachineName}." };
+            //TODO: SSML and stuff?
+        }
+
+        private object SayUnknownIntent()
+        {
+            return new { speech = "I didn't get that, could you rephrase and try again?" };
+        }
+    }
+}
---
+++ GoogleAssistantWebApi/WebApiConfig.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Http;
+using System.Net.Http.Headers;
+
+namespace GoogleAssistantWebApi
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            //route using RouteAttributes set on controllers/actions
+            config.MapHttpAttributeRoutes();
+
+            //make JSON the default response for requests w/ Accept: text/html headers (e.g. web browsers)
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        }
+    }
+}
